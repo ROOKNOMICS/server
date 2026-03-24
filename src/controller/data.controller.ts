0235a02836b485e8230a5f6ecfc4d4161c89d4a4
Sudhanshu <sudhanshumani94@gmail.com>
2026-03-25 01:59:03 +0530

    add approprite return response

--- src/controller/data.controller.ts
+++ src/controller/data.controller.ts
@@ -3,7 +3,6 @@ import  main  from '../index.js'
 export default function generateFromUser(symbol:string,startDate:string, endDate:string, capital: number, activeRules: String[]){
 
     try {
-        const 
         
         
     } catch (error) {
